7b67bb1291918d70c2f0e03fbd9682a05f7d5245
Ahamed <ahamedali95@gmail.com>
2018-10-01 13:50:25 -0400

    interface

--- tutorial/src/AbstractClass/DeerExample.java
+++ tutorial/src/AbstractClass/DeerExample.java
@@ -3,6 +3,7 @@ package AbstractClass;
 public class DeerExample {
 	public static void main(String[] args) {
 		Animal obj = new Deer("deer", 10);
+		Animal obj2 = new Deer("deer", 10);
 		obj.eat();
 		obj.sound();
 	}
