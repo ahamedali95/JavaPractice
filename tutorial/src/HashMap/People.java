9ec6455f8d481ba31496f9b80651fca832ca768a
Ahamed <ahamedali95@gmail.com>
2018-10-01 21:16:44 -0400

    hashmap

---
+++ tutorial/src/HashMap/People.java
@@ -0,0 +1,22 @@
+package HashMap;
+
+public class People {
+
+	public static void main(String[] args) {
+		HashMap<String, String> hashMap= new HashMap<String, String>();
+		
+		//Adding elements to the HashMap
+		hashMap.put("name", "ahamed");
+		
+		System.out.println("value of key `name` is " + hashMap.get("name"));
+		
+		//Iterate through a HashMap
+		hashMap.forEach((key, value) -> {
+			System.out.println(key);
+		});
+		
+		//check to see whether a key exists or not 
+		System.out.println(hashMap.containsKey("age"));
+	}
+
+}
